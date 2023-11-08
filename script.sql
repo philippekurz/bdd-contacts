168947b16fd7ef8d7151c21c42248eb4d260c3be
Philippe <philippe.kurz@gmail.com>
2023-11-08 22:44:32 +0100

    table contacts

--- script.sql
+++ script.sql
@@ -8,3 +8,17 @@ CREATE TABLE pays (
   PRIMARY KEY (iso_3)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE contacts (
+  id INT NOT NULL AUTO_INCREMENT,
+  nom VARCHAR(70) NOT NULL,
+  prenom VARCHAR(70) NOT NULL,
+  email VARCHAR(70) NOT NULL,
+  pays_iso_3 VARCHAR(3) NOT NULL,
+  PRIMARY KEY (id),
+  INDEX fk_contacts_pays_idx (pays_iso_3 ASC),
+  CONSTRAINT fk_contacts_pays
+    FOREIGN KEY (pays_iso_3)
+    REFERENCES pays (iso_3)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB DEFAULT CHARSET=utf8;
